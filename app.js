9d2f58dc7f6caa102958c09566948e6384768f08
Colin Di Meo <dimeo.colin@gmail.com>
2017-08-23 21:59:43 -0400

    second commit - style updates, still working on the lettersGuessed array carrying over on page refresh

--- app.js
+++ app.js
@@ -23,35 +23,39 @@ let count = 8;
 
 //setting up a session for each request to the page
 app.use(session({
- secret: 'foo_bar',
+ secret: 'wool socks',
  resave: false,
  saveUninitalized: true
 }));
 
+//sets up the inital load of the page
 app.get('/', function(req, res){
- res.render('index', {wordToPlay});
+ res.render('index', {wordToPlay, lettersGuessed});
  req.session.word = randomWord;
 });
 
+//posts submissions from the guess form
+//contains checkWord function to check
+//the guess input against the mystery word
 app.post('/', function(req, res){
   let guess = req.body.guess;
   checkWord(guess);
   res.render('index', {wordToPlay, lettersGuessed, count});
-  console.log(lettersGuessed);
-  console.log(guess)
-  console.log('app.post ^');
+  console.log(lettersGuessed.length);
+  if (count === 0) {
+    console.log('you lost!')
+  }
 })
 
+//listens for port 3000 on local host
 app.listen(port);
 
+//function to check user input against mystery word letters
 function checkWord(guess) {
   if (wordToPlay.includes(guess)) {
    console.log('hell yea!')
-  } else {
+ } else {
    lettersGuessed.push(guess);
    count--;
-   console.log(lettersGuessed);
-   console.log(guess)
-   console.log('checkWord^');
-  }
+ }
 }
