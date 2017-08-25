87a8dcd8cff03305b132387b4cf1a4b693fd3f87
Colin Di Meo <dimeo.colin@gmail.com>
2017-08-25 15:38:43 -0400

    fourth commit - MVP acheived. working on win/lose states and capping user input/guess count

--- app.js
+++ app.js
@@ -16,9 +16,12 @@ app.use(body.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/public'));
 
 //variables for usage in the game
+let gameLose;
+let gameWin;
 let randomWord = words[Math.floor(Math.random() * words.length)];
 let wordToPlay = [...randomWord];
-let correctGuess = [];
+let hiddenWord = [...randomWord];
+let wordDisplay = hiddenWord.fill('');
 let lettersGuessed = [];
 let count = 8;
 
@@ -31,24 +34,23 @@ app.use(session({
 
 //sets up the inital load of the page
 app.get('/', function(req, res){
- // console.log(lettersGuessed);
- res.render('index', {wordToPlay, count});
+ res.render('index', {wordToPlay, wordDisplay, count});
  req.session.word = randomWord;
  console.log(wordToPlay);
+ console.log('wordToPlay^');
+ console.log(wordDisplay);
+ console.log('wordDisplay^');
 });
 
 //posts submissions from the guess form
 //contains checkWord function to check
 //the guess input against the mystery word
 app.post('/', function(req, res){
-  let guess = req.body.guess;
+  let guess = req.body.guess.toLowerCase();
   checkWord(guess);
-  res.render('index', {wordToPlay, correctGuess, lettersGuessed, count});
-  console.log(lettersGuessed);
-  console.log(req.body.guess);
-  if (count === 0) {
-    console.log('you lost!')
-  }
+  gameOver();
+  gameWon();
+  res.render('index', {wordToPlay, wordDisplay, lettersGuessed, count, gameLose, gameWin});
 })
 
 //listens for port 3000 on local host
@@ -57,11 +59,53 @@ app.listen(port);
 //function to check user input against mystery word letters
 function checkWord(guess) {
   if (wordToPlay.includes(guess)) {
-   correctGuess.push(guess);
-   console.log(correctGuess);
-   console.log('hell yea!')
+      let correctGuess = wordToPlay.indexOf(guess);
+      while (~correctGuess) {
+      hiddenWord[correctGuess] = guess;
+      correctGuess = wordToPlay.indexOf(guess, correctGuess + 1);
+      console.log(hiddenWord);
+      console.log('hiddenWord^');
+      console.log(correctGuess);
+      console.log('correctGuess^');
+      console.log(guess);
+      console.log('guess^');
+      console.log(hiddenWord.length);
+      console.log('hiddenWord.length^');
+    }
  } else {
    lettersGuessed.push(guess);
    count--;
  }
 }
+
+function gameOver() {
+  if (count <= 0) {
+    gameLose = "You've lost!"
+    count = "No more guesses!"
+  }
+}
+
+function gameWon() {
+  if (hiddenWord === wordToPlay) {
+    gameWin = "You've won!"
+  }
+}
+
+// graveyard
+
+// function revealLetters(guess) {
+//   for (let i = 0; i < wordToPlay.length; i++) {
+//     if (wordToPlay[i].includes(guess)) {
+//       correctGuess.push(guess);
+//       console.log(wordToPlay);
+//       console.log('test');
+//       console.log(correctGuess);
+//       console.log(wordToPlay[i]);
+//     }
+//   }
+// }
+
+// counter w/ lose message
+// if (count === 0) {
+//   console.log('you lost!')
+// }
