2b87af05a124893d3214c9eb50c42b9b41c84146
Colin Di Meo <dimeo.colin@gmail.com>
2017-08-26 08:59:15 -0400

    fifth commit - win & lose states are active

--- app.js
+++ app.js
@@ -37,9 +37,6 @@ app.get('/', function(req, res){
  res.render('index', {wordToPlay, wordDisplay, count});
  req.session.word = randomWord;
  console.log(wordToPlay);
- console.log('wordToPlay^');
- console.log(wordDisplay);
- console.log('wordDisplay^');
 });
 
 //posts submissions from the guess form
@@ -56,41 +53,48 @@ app.post('/', function(req, res){
 //listens for port 3000 on local host
 app.listen(port);
 
-//function to check user input against mystery word letters
+// function to compare user guess against the wordToPlay
+// if user input matches any of the letters, they are
+// assigned to the corresponding index
+// position in the hiddenWord/wordDisplay
 function checkWord(guess) {
+ //checks if the word in play includes the users input
   if (wordToPlay.includes(guess)) {
+    // correctGuess is assignsed the index positions which
+    // match the users input, in this case it grabs the first
+    // of multiple matches, or just a single match if there's
+    // only one possible match.
       let correctGuess = wordToPlay.indexOf(guess);
+      // '~' operator checks if correctGuess is NOT equal to -1
+      // when used in conjunction with 'while' I think it
+      // iterates over for every instance which user input is equal to
+      // multiple indices whose values greater than -1. In this case
+      // it acts as a catch for any subsequent instances of matches being made.
       while (~correctGuess) {
+      // displays matches on game board
       hiddenWord[correctGuess] = guess;
+      // resets correctGuess value to -1 to break the while loop if
+      // there are no more possible matches.
       correctGuess = wordToPlay.indexOf(guess, correctGuess + 1);
-      // console.log(hiddenWord);
-      // console.log('hiddenWord^');
-      // console.log(correctGuess);
-      // console.log('correctGuess^');
-      // console.log(guess);
-      // console.log('guess^');
-      // console.log(wordToPlay);
-      // console.log('wordToPlay^');
-      // console.log(wordDisplay);
-      // console.log('wordDisplay^');
     }
  } else {
+   // pushes incorrect guesses to be displayed
    lettersGuessed.push(guess);
+   // reduces count by 1
    count--;
  }
 }
 
 function gameOver() {
-  if (count <= 0) {
+  while (count <= 0) {
     gameLose = "You've lost!"
     count = "No more guesses!"
   }
 }
 
 function gameWon() {
-    if (wordDisplay.indexOf(wordToPlay)) {
+    if (!wordDisplay.includes('')) {
       gameWin = "You've won!"
-      console.log('test this bitch out!')
     }
   }
 
